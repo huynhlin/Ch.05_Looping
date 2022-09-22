4cc7e4fec51e7af5ae0266e08f3cf117a3624c38
huynhlin <huynhlin@urbandaleschools.com>
2022-09-21 22:04:23 -0500

    5.2 done that was quite challenging i had to completely scrap my code once L

--- 5.2_Roshambo.py
+++ 5.2_Roshambo.py
@@ -12,13 +12,75 @@ When the user quits print an end game message and their win/loss/tie record
 
 '''
 
+import random
 
+print("Let's play rock paper scissors. Type 1 for rock, 2 for paper, and 3 for scissors. Enter a 4 at any time if you would like to quit.")
 
+q = False
+wincount = 0
+losscount = 0
+tiecount = 0
 
+while q == False:
+    pc = random.randrange(1, 4)
+    uc = int(input("What is your choice?"))
+    if uc == 1:
+        print("Your choice is rock.")
+        if pc == 2:
+            losscount = losscount+1
+            print("The computer chose paper. That means one point for the computer! You now have:",losscount,
+                  "losses! Better luck next time.")
+            print("Onto the next round!")
+        elif pc == 3:
+            wincount = wincount+1
+            print("The computer chose scissors. According to the rules that nets you one more point! You now have:",wincount,
+                  "wins! Congratulations player, you'll be in the big leagues in no time.")
+            print("Onto the next round!")
+        else:
+            tiecount = tiecount+1
+            print("The computer chose rock. Jinx! You guys tied! There have been:",tiecount,
+                  "ties this session. Try being a little more original.")
+            print("Onto the next round!")
+    elif uc == 2:
+        print("Your choice is paper.")
+        if pc == 3:
+            losscount = losscount+1
+            print("The computer chose scissors. That means one point for the computer! You now have:",losscount,
+                  "losses! Better luck next time.")
+            print("Onto the next round!")
+        elif pc == 2:
+            tiecount = tiecount+1
+            print("The computer chose paper. Jinx! You guys tied! There have been:",tiecount,
+                  "ties this session. Try being a little more original.")
+            print("Onto the next round!")
+        else:
+            wincount = wincount+1
+            print("The computer chose rock. According to the rules that nets you one more point! You now have:",wincount,
+                  "wins! Congratulations player, you'll be in the big leagues in no time.")
+            print("Onto the next round!")
 
-
-
-
-
-
-
+    elif uc == 3:
+        print("Your choice is scissors.")
+        if pc == 2:
+            wincount = wincount + 1
+            print("The computer chose paper. According to the rules that nets you one more point! You now have:",
+                  wincount, "wins! Congratulations player, you'll be in the big leagues in no time.")
+            print("Onto the next round!")
+        elif pc == 3:
+            tiecount = tiecount + 1
+            print("The computer chose scissors. Jinx! You guys tied! There have been:", tiecount,
+                  "ties this session. Try being a little more original.")
+            print("Onto the next round!")
+        else:
+            losscount = losscount + 1
+            print("The computer chose rock. That means one point for the computer! You now have:", losscount,
+                  "losses! Better luck next time.")
+            print("Onto the next round!")
+    else:
+        if wincount == 0 and losscount == 0 and tiecount == 0:
+            print("You didn't even play! Try your luck before quitting.")
+        else:
+            winrate = wincount/(losscount+wincount)
+            percent = winrate*100
+            print("Thank you for playing. You had a total of:", wincount,"wins,",losscount,"losses, and",tiecount,
+                  "ties. That adds up to a winrate of:",percent,"percent. Thanks for playing!")
